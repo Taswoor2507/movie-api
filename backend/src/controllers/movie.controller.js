4f8ec5c343e301a1fcc845575f1b05eadb0ff339
shimputa <waseemkhyber123@gmail.com>
2024-07-24 19:15:12 +0500

    add review and rating feature

--- backend/src/controllers/movie.controller.js
+++ backend/src/controllers/movie.controller.js
@@ -1,6 +1,7 @@
 import Movie from '../models/movies.model.js';
 import omdbService from '../services/omdb.service.js';
 import ApiError from '../utils/ErrorHandler.util.js';
+import User from '../models/user.model.js';
 
 const searchMovie = async (req, res, next) => {
   const { title } = req.query;
@@ -24,9 +25,6 @@ const searchMovie = async (req, res, next) => {
       
       return next(new ApiError(404, 'Movie not found'));
     }
-
-    // // Log movie data before saving
-    // console.log('Movie Data:', movieData);
 
     // Parse runtime
     const runtimeMinutes = parseInt(movieData.Runtime.split(' ')[0]) || 0;
@@ -66,25 +64,90 @@ const searchMovie = async (req, res, next) => {
     return next(new ApiError(500 , "An error occurred while searching for the movie" ))
   }
 };
+const getMovieById = async (req, res, next) => {
+  const { id } = req.params;
 
+  try {
+    const movie = await Movie.findById(id);
+    if (!movie) {
+      return next(new ApiError(404 , 'Movie not found'));
+    }
+
+    res.json(movie);
+  } catch (error) {
+    // console.error('Error fetching movie by ID:', error);
+    return next(new ApiError(500 , 'An error occurred while fetching the movied'));
+  }
+};
+
+const rateMovie = async (req, res, next) => {
+  const { id } = req.params;
+  const { rating, review } = req.body;
+
+  if (!rating || !review) {
+    return next(new ApiError(400 , 'Rating and review are required'));
+  }
+
+  try {
+    const user = await User.findById(req.user);
+    if (!user) {
+      return next(new ApiError(401 , 'User not found'));
+    }
+
+    const movie = await Movie.findById(id);
+    if (!movie) {
+      return next(new ApiError(404 , 'Movie not found'));
+    }
+
+    let totalRating = movie.reviews.reduce((acc, review) => acc + review.rating, 0);
+    const existingReviewIndex = movie.reviews.findIndex(r => r.user.toString() === req.user);
+
+    if (existingReviewIndex !== -1) {
+      // Update existing review
+      totalRating -= movie.reviews[existingReviewIndex].rating;
+      movie.reviews[existingReviewIndex].rating = rating;
+      movie.reviews[existingReviewIndex].comment = review;
+    } else {
+      // Add new review
+      const newReview = {
+        user: req.user,
+        name: user.username,
+        rating: rating,
+        comment: review,
+      };
+      movie.reviews.push(newReview);
+      movie.noOfReviews += 1;
+    }
+
+    totalRating += rating;
+    // Recalculate the average rating
+    movie.ratings = totalRating / movie.reviews.length;
+
+    await movie.save();
+
+    res.json(movie);
+  } catch (error) {
+    // console.error('Error adding or updating review:', error);
+    return next(new ApiError(500 , 'An error occurred while adding or updating the rating and review'));
+  }
+};
 
 const getMovies = async (req, res, next) => {
   const { genre } = req.query;
   const query = genre ? { genre: { $in: [genre] } } : {};
 
   try {
-    // Log the query to verify
-    // console.log('Genre Query:', query);
-
-    const movies = await Movie.find(query);
+   
+     const movies = await Movie.find(query);
     res.json(movies);
   } catch (error) {
     return next(new ApiError(500, "An error occurred while retrieving movies"))
   }
 };
 
-
 export default {
     searchMovie,
     getMovies,
+    rateMovie,
+    getMovieById,
   };
