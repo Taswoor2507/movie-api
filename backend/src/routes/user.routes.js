a7d19bba1e33926a3b02bfb1d329752eed03be85
Israr Hussain <isroo0522@gmail.com>
2024-07-29 16:50:35 +0500

    create-login-routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -4,12 +4,14 @@ import userController from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
 router.get('/verify/:userId', userController.verifyUser);
 router.post('/activate/:userId', userController.activateUser);
 router.get('/all', userController.findAllUsers);
-router.delete('/:userId', userController.deleteUser);
+router.delete('/:userId', userController.deleteUser) ;
 router.put('/:userId', userController.updateUser);
 router.post('/refresh-token', userController.refreshAccessToken);
+// router.delete('/:userId', userController.removeUser);
 
 export default router;
     
